9c079e4e338fe96715913c5963d2e797698b4b90
Ian McElroy <mcelroyian@gmail.com>
2020-07-22 22:01:31 -0500

    wip

--- sliding_window_max/sliding_window_max.py
+++ sliding_window_max/sliding_window_max.py
@@ -4,13 +4,26 @@ Returns: a List of integers
 '''
 def sliding_window_max(nums, k):
     # Your code here
+    #two pointers, index and k
+    #for each item look ahead until k saving the highest value
+    res = []
+    end = len(nums) - k+ 1
+    for i in range(end):
+        max = nums[i]
+        for j in range(i, k+i if k+i < len(nums) else len(nums)):
+            if nums[j] > max:
+                max = nums[j]
+        res.append(max)
+    return res
 
-    pass
+    
+
+
 
 
 if __name__ == '__main__':
     # Use the main function here to test out your implementation 
     arr = [1, 3, -1, -3, 5, 3, 6, 7]
-    k = 3
+    k = 2
 
     print(f"Output of sliding_window_max function is: {sliding_window_max(arr, k)}")
